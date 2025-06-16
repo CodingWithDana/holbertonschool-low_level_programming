b19b2dae58a4f43563be7703cf45cd5de9d4bf1a
CodingWithDana <danadaole@gmail.com>
2025-06-16 13:06:52 +0000

    print exactly Programming is like building a multilingual puzzle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * description: use the puts fuction to print a message to the standard output
+ *
+ * return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
