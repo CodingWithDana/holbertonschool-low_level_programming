c3960d624978d6bb7994a9060decc80c8f78884d
CodingWithDana <danadaole@gmail.com>
2025-06-17 06:18:14 +0000

    print random number with condition - 2

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -10,31 +10,29 @@
  *
  * Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
-  int last_digit;
+	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	/* your code goes there */
 	printf("Last digit of %d is %d ", n, last_digit);
 
 	if (last_digit > 5)
-	  {
-	    printf("and is greater than 5\n");
-	  }
+	{
+		printf("and is greater than 5\n");
+	}
 	else if (last_digit == 0)
-	  {
-	    printf("and is 0\n");
-	  }
+	{
+		printf("and is 0\n");
+	}
 	else
-	  {
-	    printf("and is less than 6 and not 0\n");
-	  }
+	{
+		printf("and is less than 6 and not 0\n");
+	}
 
 	return (0);
 }
