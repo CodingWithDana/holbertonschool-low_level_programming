8c3b2b972b4d1d516aef65bc07443935c59e9a01
CodingWithDana <danadaole@gmail.com>
2025-06-17 06:41:35 +0000

    print alphabet in lowercase - add comments

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**                                                                                                                                
+ * main - Entry point                                                        
+ *                                                                                                                                 
+ * Description: prints the alphabet in lowercase followed by a new line                                                             *                                                                                                   
+ * Return: Always 0 (Success)                                                                                                      
+ */
 int main(void)
 {
 	char c = 'a';
