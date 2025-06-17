1ed64a4916d76f893567452ced9a836ccb43aac7
CodingWithDana <danadaole@gmail.com>
2025-06-17 05:10:03 +0000

    print random number and classify positive, negative or zero - 3

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-include <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
@@ -11,24 +11,24 @@ include <stdlib.h>
  */
 int main(void)
 {
-int n;
+        int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-printf("%d ", n);
-if (n > 0)
-{
-printf("is positive\n");
-}
-else if (n == 0)
-{
-printf("is zero\n");
-}
-else
-{
-printf("is negative\n");
-}
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        printf("%d ", n);
+        if (n > 0)
+          {
+            printf("is positive\n");
+          }
+        else if (n == 0)
+          {
+            printf("is zero\n");
+          }
+        else
+          {
+            printf("is negative\n");
+          }
 
-return (0);
+        return (0);
 }
