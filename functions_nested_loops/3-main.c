4267bebc0bf026e49a7d69dbcf265a1b4acbbee3
CodingWithDana <danadaole@gmail.com>
2025-06-17 14:39:34 +0000

    check a character for lowercase

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	printf("%d", r);
+	r = _islower('o');
+	printf("%d", r);
+	r = _islower(108);
+	printf("%d\n", r);
+	
+	return (0);
+}
