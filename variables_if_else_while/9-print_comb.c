33f0fe2e6405bbc0b5c3826de62be59bd3cc6d21
CodingWithDana <danadaole@gmail.com>
2025-06-17 13:07:39 +0000

    print all combination of single digit numbers

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: print all combinations of single digit numbers
- * using only putchar 4 times
+ * Description: Print all combinations of single-digit numbers
+ * using only putchar 4 times, separated by ", "
  *
  * Return: Always 0
  */
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < 10)
-    {
-        putchar(i + '0');          
-        if (i < 9)
-        {
-            putchar(',');           
-            putchar(' ');         
-        }
-        i++;
-    }
-    putchar('\n');                 
-    return 0;
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
