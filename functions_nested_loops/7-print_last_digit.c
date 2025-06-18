f596c1b1521388328addee70661f2afdfc88ac94
CodingWithDana <danadaole@gmail.com>
2025-06-18 06:48:51 +0000

    print last digit of a number - change printf to putchar as requested

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,6 +14,7 @@ int print_last_digit(int n)
     if (last_digit < 0)
         last_digit = -last_digit;
 
-    printf("%d", last_digit);
+    _putchar( '0' + last_digit);
+    
     return last_digit;
 }
