56e36173f85afb119fa4d8f4baac6b0573a5b28e
CodingWithDana <danadaole@gmail.com>
2025-06-18 06:50:26 +0000

    print last digit of a number - correct Betty styling

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_last_digit - Print the last digit of a number
- * @n: The integer to check
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to extract the last digit from
  *
- * Return: The last digit of the number
+ * Return: The value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-    _putchar( '0' + last_digit);
-    
-    return last_digit;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
