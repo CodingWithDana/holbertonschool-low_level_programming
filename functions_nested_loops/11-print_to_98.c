5b4b6828b32f0de018a77279c7e5b47ece483539
CodingWithDana <danadaole@gmail.com>
2025-06-18 13:45:13 +0000

    print all natural numbers - correct styling

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,55 +1,50 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: starting number
- *
- * Description: Numbers are printed in order, separated by commas
- * and spaces, ending with a new line.
+ * print_number - Prints an integer using _putchar
+ * @n: Integer to print
  */
-
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n / 10)
-        print_number(n / 10);
+	if (n / 10)
+		print_number(n / 10);
 
-    _putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }
 
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            print_number(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            print_number(i);
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
