a37f489fb4e233d6dc28ae02f793a5715b56509a
CodingWithDana <danadaole@gmail.com>
2025-06-23 11:52:54 +0000

    print a straight line - betty style

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_line - Prints a straight line
- * of underscores without space 
- * @n : number of underscores to print
+ *               of underscores without space
+ * @n: Number of underscores to print
+ *
  * Return: void
  */
-
 void print_line(int n)
 {
-  int i = 0;
+	int i = 0;
 
-  while (i < n)
-    {
-      _putchar('_');
-      i++;
-    }
-  _putchar('\n');
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
 }
-  
