25396c881c4840dc480896a9322b4758a39dbd0e
CodingWithDana <danadaole@gmail.com>
2025-06-23 08:06:40 +0000

    check for uppercase character - correct Betty style

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-/**                                                                                                              
- * _isupper - check for uppercase character                                               
- * @c: the character to check                                                                         
- * Return: 1 if uppercase, 0 otherwise        
+/**
+ * _isupper - Check for uppercase character
+ * @c: The character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    return(1);
-  else
-    return(0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-
