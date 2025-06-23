d1f2be4d7a1fac23f066777802f3d64a671f18cb
CodingWithDana <danadaole@gmail.com>
2025-06-23 12:22:58 +0000

    print diagonal lines

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,25 +9,25 @@
  */
 void print_diagonal(int n)
 {
-  int i = 0;
-  int j;
+	int i = 0;
+	int j;
 
-  if (n <= 0)
-  {
-    _putchar('\n');
-    return;
-  }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-  while (i < n)
-  {
-    j = 0;
-    while (j < i)
-    {
-      _putchar(' ');
-      j++;
-    }
-    _putchar('\\');
-    _putchar('\n');
-    i++;
-  }
+	while (i < n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
 }
