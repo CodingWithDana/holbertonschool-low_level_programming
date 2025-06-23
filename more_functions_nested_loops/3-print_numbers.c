3ddeca932e188e1ec9334ede1ecab4d8a1c41f58
CodingWithDana <danadaole@gmail.com>
2025-06-23 08:43:45 +0000

    print 0 to 9 using putchar twice

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * followed by a new line
+ *
+ * Return: 0 on success
+ */
+
+void print_numbers(void)
+{
+  int i;
+
+  for (i = 0; i < 10; i++)
+  {
+      putchar('0' + i);
+  }
+
+  putchar('\n');
+}
+  
