b34f11c6a9b36e007d87cf6e9e19ee469a3040ed
CodingWithDana <danadaole@gmail.com>
2025-06-25 05:35:15 +0000

    prints a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts - prints a string
- * @str: the string to print 
- *
+ * _puts - prints a string followed by a new line
+ * to stdout
+ * @str: the string to be printed 
+ * Description: this function prints each character of the input string
  * Return: void
  */
 
 void _puts(char *str)
 {
-puts(str);
+while (*str != '\0')
+{
+	_putchar(*str);
+	str++;
+}
+_putchar('\n');
 }
