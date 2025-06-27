b1ffcd8f69d3976e1b44a53570d3f5f96476f484
CodingWihDana <danadaole@gmail.com>
2025-06-27 11:00:53 +0000

    convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <limits.h>
 /**
  * _atoi - Converts a string to an integer.
  * @s: The input string to convert.
@@ -27,6 +27,15 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num_started = 1;
+
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
 			result = result * 10 + (s[i] - '0');
 		}
 		else if (num_started)
