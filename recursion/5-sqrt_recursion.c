6fe524c948597cfdfd3be84de46104b0f46381e9
CodingWithDana <danadaole@gmail.com>
2025-07-02 11:10:31 +0000

    returns the natural square root of a number

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,39 +1,38 @@
 #include "main.h"
+
+/**
+ * _sqrt_checker - recursively checks for the natural square root of n
+ * @n: the number to find the square root of
+ * @current_number: the current number to be checked (starting from 0)
+ * Return: the natural square root
+ */
+int _sqrt_checker(int n, int current_number)
+{
+	/* If n has a natural square root number, return that number */
+	if (current_number * current_number == n)
+	{
+		return (current_number);
+	}
+
+	/* If the multiplication passed n, return -1 (case error) */
+	if (current_number * current_number > n)
+	{
+		return (-1);
+	}
+
+	return (_sqrt_checker(n, current_number + 1));
+}
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: a positive number to be checked if it has a natural square root
+ * @n: the number to find the square root
  * Return: the natural square root of a number
  */
 int _sqrt_recursion(int n)
 {
-	static int natural_square_root_number;
-
-	/* If n doesn't have a natural square root number, return -1 (case error) */
 	if (n < 0)
 	{
 		return (-1);
 	}
-
-	/* If n has a natural square root number, return that number */
-	if (natural_square_root_number * natural_square_root_number == n)
-	{
-		/* Save the correct value to the variable */
-		int result = natural_square_root_number;
-		/* Reset for future recursive calls */
-		natural_square_root_number = 0;
-		return (result);
-	}
-
-	/* If passed n, return -1 (case error) */
-	if (natural_square_root_number * natural_square_root_number > n)
-	{
-		/* Reset for future recursive calls */
-		natural_square_root_number = 0;
-		return (-1);
-	}
-
-	natural_square_root_number++;
-
-	/* Recursive calls */
-	return (_sqrt_recursion(n));
+	return (_sqrt_checker(n, 0));
 }
