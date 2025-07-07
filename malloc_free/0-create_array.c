5609ca16fc4399afe82e7909ba7258b5540e4e62
CodingWithDana <danadaole@gmail.com>
2025-07-07 03:27:02 +0000

    create an array of characters, initializing it with a specific char - fix betty styling

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - creates an array of chars and initializes it with a specific char
+ * create_array - creates an array of chars and
+ * initializes it with a specific char
  * @size: size of the array
  * @c: the specific char to initialize the array with
  * Return: pointer to the array, or NULL if the program fails or size is 0
