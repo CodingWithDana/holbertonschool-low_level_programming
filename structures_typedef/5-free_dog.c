86205f5796cea3104fb5ad83e10c0e287c70e266
CodingWithDana <danadaole@gmail.com>
2025-07-10 09:01:45 +0000

    frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dogs - frees dogs
+ * *d: pointer to the dog to free
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	/* If pointer d is not NULL */
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
