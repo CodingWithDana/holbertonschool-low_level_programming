2069f16eb6518a52c2479da3451e405559339b76
CodingWithDana <danadaole@gmail.com>
2025-07-28 07:28:58 +0000

    prints the nubmer of elements in the linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - prints the number of elements in the linked list
+ * @h: pointer to the head of the list
+ * Return: the number of elements in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			return (-1);
+		else
+			count++;
+		h = h->next;
+	}
+	return (count);
+}
