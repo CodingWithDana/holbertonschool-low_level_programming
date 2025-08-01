2a831863b8f4325f3a1c38859b11a828c1086f67
CodingWithDana <danadaole@gmail.com>
2025-08-01 07:14:32 +0000

    insert a new node at a given index - fix coding style errors

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,8 @@
  * insert_dnodeint_at_index - inserts a new node at a given index
  * @h: pointer pointing to the head pointer of the doubly linked list
  * @idx: index where the new node should be inserted (starting at 0)
- * @n: Data to store in the new node
+ * @n: data to store in the new node
+ * Return: the address of the new node, otherwise NULL if insertion fails
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -16,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	/* Move through the list to reach the node just before the target index */ 
+	/* Move through the list to reach the node just before the target index */
 	while (current != NULL && i < (idx - 1))
 	{
 		current = current->next;
@@ -25,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/* Check if reach the end of list before idx - 1 */
 	if (current == NULL)
 		return (NULL);
-	
+
 	/* Insert at the end */
 	if (current->next == NULL)
 		return (add_dnodeint_end(h, n));
@@ -37,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->prev = current;
-	new_node->next = current-> next;
+	new_node->next = current->next;
 
 	current->next->prev = new_node;
 	current->next = new_node;
