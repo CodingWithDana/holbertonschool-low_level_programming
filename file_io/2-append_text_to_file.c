6d7191c56fc10b77ae3d357ec2197db07d100996
Dana Le <danadaole@gmail.com>
2025-08-02 22:52:11 +1000

    append text at the end of a file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -18,12 +18,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
-		
+
 		written = write(fd, text_content, len);
 		if (written == -1 || (size_t)written != len)
 		{
