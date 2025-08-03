d4c2cdf2927fe61a299145edd2ee3616f763baac
Dana Le <danadaole@gmail.com>
2025-08-03 20:35:03 +1000

    fix indentation errors p3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,22 +27,29 @@ void error_exit(int code, const char *message, const char *arg)
   */
 int main(int argc, char *argv[])
 {
-	int fd_from; int fd_to = -1;
-	ssize_t read_bytes; ssize_t written_bytes; ssize_t total_written;
+	int fd_from;
+	int fd_to = -1;
+	ssize_t read_bytes;
+	ssize_t written_bytes;
+	ssize_t total_written;
 	char buffer[BUFFER_SIZE];
 	struct stat st_from, st_to;
 	int file_to_exists = 0;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	if (stat(argv[1], &st_from) == -1)
         error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
 	if (stat(argv[2], &st_to) == 0)
 		file_to_exists = 1;
 	else if (errno != ENOENT)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
 	if (file_to_exists && 
 		st_from.st_ino == st_to.st_ino &&
 		st_from.st_dev == st_to.st_dev)
@@ -52,15 +59,18 @@ int main(int argc, char *argv[])
 			argv[1], argv[2]);
 		exit(100);
 	}
+
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
 	read_bytes = read(fd_from, buffer, BUFFER_SIZE);
 	if (read_bytes == -1)
 	{
 		close(fd_from);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
+
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -70,6 +80,7 @@ int main(int argc, char *argv[])
 	}
 	if (!file_to_exists)
 		fchmod(fd_to, 0664);
+
 	while (read_bytes > 0)
 	{
         total_written = 0;
@@ -85,6 +96,7 @@ int main(int argc, char *argv[])
 			}
 			total_written += written_bytes;
 		}
+
 		read_bytes = read(fd_from, buffer, BUFFER_SIZE);
 		if (read_bytes == -1)
 		{
@@ -93,6 +105,7 @@ int main(int argc, char *argv[])
 			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 		}
 	}
+	
     if (close(fd_from) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
