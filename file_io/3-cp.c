02f2d0bd6279d2c92bd0d18c187f3b528e78520c
Dana Le <danadaole@gmail.com>
2025-08-03 15:55:31 +1000

    copy content of a file to another file - fix output errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	if (close(fd_to) == -1)
+	if (fd_to != -1 && close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
