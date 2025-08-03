ba4a4a42832e7c6dfe67aa9a639c95123fc633c2
Dana Le <danadaole@gmail.com>
2025-08-03 16:17:31 +1000

    copy content of a file to another file - v2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+
 #define BUFFER_SIZE 1024
+
 /**
   * error_exit - handle error messages and exit with given code
   * @code: exit code (97, 98, 99, 100)
@@ -36,87 +38,72 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
+	}
+	
+	if (stat(argv[1], &st_from) == -1)
+        	error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (stat(argv[2], &st_to) == 0)
+		file_to_exists = 1;
+	else if (errno != ENOENT)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
+	if (file_to_exists && st_from.st_ino == st_to.st_ino && st_from.st_dev == st_to.st_dev)
+	{
+		dprintf(STDERR_FILENO, "Error: %s and %s are the same file\n", argv[1], argv[2]);
+		exit(100);
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	/* Check if file_to exists and is the same as file_from */
-	if (stat(argv[2], &st_to) == 0)
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		file_to_exists = 1;
-		/* Check if both paths point to the same file */
-		fstat(fd_from, &st_from);
-		if (st_from.st_ino == st_to.st_ino && st_from.st_dev == st_to.st_dev)
-		{
-			close(fd_from);
-			dprintf(STDERR_FILENO, "Error: %s and %s are the same file\n", argv[1], argv[2]);
-			exit(100);
-		}
+		close(fd_from);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
-	
-	fd_to = -1;
-	while (1)
+
+	if (!file_to_exists)
+		fchmod(fd_to, 0664);
+
+	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		read_bytes = read(fd_from, buffer, BUFFER_SIZE);
-
-        	if (read_bytes == -1)
-        	{
-                	close(fd_from);
-                	if (fd_to != -1) close(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		/* end of file */
-		if (read_bytes == 0)
-			break;
-
-		if (fd_to == -1)
-		{
-			fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-			if (fd_to == -1)
-			{
-				close(fd_from);
-				if (errno == EACCES)
-				{
-					dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-					return (0);
-				}
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			if (!file_to_exists)
-				fchmod(fd_to, 0664);
-		}
-
-		total_written = 0;
-		while (total_written < read_bytes)
+        	total_written = 0;
+        	while (total_written < read_bytes)
 		{
 			written_bytes = write(fd_to, buffer + total_written, read_bytes - total_written);
-			if (written_bytes == -1)
-			{
+            		if (written_bytes == -1)
+            		{	
 				close(fd_from);
 				close(fd_to);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			total_written += written_bytes;
+				error_exit(99, "Error: Can't write to %s\n", argv[2]);
+            		}
+		total_written += written_bytes;
 		}
-	}	
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
 	}
 
-	if (fd_to != -1 && close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-	return (0);
+	if (read_bytes == -1)
+    	{
+        	close(fd_from);
+        	close(fd_to);
+        	error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+    	}
+
+    	if (close(fd_from) == -1)
+    	{
+        	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        	exit(100);
+    	}
+
+    	if (close(fd_to) == -1)
+    	{
+        	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        	exit(100);
+    	}
+
+    	return (0);
 }
