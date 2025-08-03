55e38085e6c6c8f3a8933632542c2d150494e5d0
Dana Le <danadaole@gmail.com>
2025-08-03 20:29:25 +1000

    fix indentation errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,13 +10,13 @@
 /**
   * error_exit - handle error messages and exit with given code
   * @code: exit code (97, 98, 99, 100)
-  * @message: error message format string 
+  * @message: error message format string
   * @arg: additional arguments (filename, file descriptor)
   */
 void error_exit(int code, const char *message, const char *arg)
 {
 	dprintf(STDERR_FILENO, message, arg);
-	exit (code);
+	exit(code);
 }
 
 /**
@@ -27,49 +27,40 @@ void error_exit(int code, const char *message, const char *arg)
   */
 int main(int argc, char *argv[])
 {
-	int fd_from;
-    int fd_to = -1;
-	ssize_t read_bytes;
-	ssize_t written_bytes;
-	ssize_t total_written;
+	int fd_from, int fd_to = -1;
+	ssize_t read_bytes, ssize_t written_bytes, ssize_t total_written;
 	char buffer[BUFFER_SIZE];
 	struct stat st_from, st_to;
 	int file_to_exists = 0;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	if (stat(argv[1], &st_from) == -1)
         error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	if (stat(argv[2], &st_to) == 0)
 		file_to_exists = 1;
 	else if (errno != ENOENT)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
-	if (file_to_exists && st_from.st_ino == st_to.st_ino && st_from.st_dev == st_to.st_dev)
+	if (file_to_exists && 
+		st_from.st_ino == st_to.st_ino &&
+		st_from.st_dev == st_to.st_dev)
 	{
-		dprintf(STDERR_FILENO, "Error: %s and %s are the same file\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO,
+			"Error: %s and %s are the same file\n",
+			argv[1], argv[2]);
 		exit(100);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-	/* First real read: PRECHECK */
 	read_bytes = read(fd_from, buffer, BUFFER_SIZE);
 	if (read_bytes == -1)
 	{
 		close(fd_from);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 	}
-	/* END: read check */
-
-	/* Only open fd_to after confirming source is readable */
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -77,11 +68,8 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	if (!file_to_exists)
 		fchmod(fd_to, 0664);
-
-	/* Copy the already-read buffer (if any)*/
 	while (read_bytes > 0)
 	{
         total_written = 0;
@@ -94,10 +82,9 @@ int main(int argc, char *argv[])
 				close(fd_to);
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 				exit(99);
-            }
+			}
 			total_written += written_bytes;
 		}
-		/* Next read */
 		read_bytes = read(fd_from, buffer, BUFFER_SIZE);
 		if (read_bytes == -1)
 		{
@@ -106,7 +93,6 @@ int main(int argc, char *argv[])
 			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 		}
 	}
-
     if (close(fd_from) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
