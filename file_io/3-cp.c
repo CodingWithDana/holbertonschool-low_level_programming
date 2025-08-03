c844c7254bf9e0ebaebd477ccbe556868536d799
Dana Le <danadaole@gmail.com>
2025-08-03 18:16:38 +1000

    fix read return value

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,93 +37,101 @@ int main(int argc, char *argv[])
 	int file_to_exists = 0;
 	int first_read = 1;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	
-	if (stat(argv[1], &st_from) == -1)
-        	error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
 
-	if (stat(argv[2], &st_to) == 0)
-		file_to_exists = 1;
-	else if (errno != ENOENT)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+    if (stat(argv[1], &st_from) == -1)
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	if (file_to_exists && st_from.st_ino == st_to.st_ino && st_from.st_dev == st_to.st_dev)
-	{
-		dprintf(STDERR_FILENO, "Error: %s and %s are the same file\n", argv[1], argv[2]);
-		exit(100);
-	}
+    if (stat(argv[2], &st_to) == 0)
+        file_to_exists = 1;
+    else if (errno != ENOENT)
+        error_exit(99, "Error: Can't write to %s\n", argv[2]);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+    if (file_to_exists &&
+        st_from.st_ino == st_to.st_ino &&
+        st_from.st_dev == st_to.st_dev)
+    {
+        dprintf(STDERR_FILENO, "Error: %s and %s are the same file\n", argv[1], argv[2]);
+        exit(100);
+    }
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		dprintf(STDERR_FILENO, "Error abcxyz: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	if (!file_to_exists)
-		fchmod(fd_to, 0664);
+    /* First read before creating output file */
+    read_bytes = read(fd_from, buffer, BUFFER_SIZE);
+    if (read_bytes == -1)
+    {
+        close(fd_from);
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+    }
 
-	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		if (first_read)
-		{
-			fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-			if (fd_to == -1)
-			{
-				close(fd_from);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			if (!file_to_exists)
-				fchmod(fd_to, 0664);
-			
-			first_read = 0;
-		}
-		
-        	total_written = 0;
-        	while (total_written < read_bytes)
-		{
-			written_bytes = write(fd_to, buffer + total_written, read_bytes - total_written);
-            		if (written_bytes == -1)
-            		{	
-				close(fd_from);
-				close(fd_to);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-            		}
-			total_written += written_bytes;
-		}
-	}
+    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        close(fd_from);
+        error_exit(99, "Error: Can't write to %s\n", argv[2]);
+    }
 
-	if (read_bytes == -1)
-    	{
-        	close(fd_from);
-		if (fd_to != -1)
-			close(fd_to);
-        	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-    	}
+    if (!file_to_exists)
+        fchmod(fd_to, 0664);
 
-    	if (close(fd_from) == -1)
-    	{
-        	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        	exit(100);
-    	}
+    /* If source file is empty, we're done */
+    if (read_bytes == 0)
+    {
+        close(fd_from);
+        close(fd_to);
+        return (0);
+    }
 
-    	if (close(fd_to) == -1)
-    	{
-        	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        	exit(100);
-    	}
+    /* Write first chunk */
+    total_written = 0;
+    while (total_written < read_bytes)
+    {
+        written_bytes = write(fd_to, buffer + total_written, read_bytes - total_written);
+        if (written_bytes == -1)
+        {
+            close(fd_from);
+            close(fd_to);
+            error_exit(99, "Error: Can't write to %s\n", argv[2]);
+        }
+        total_written += written_bytes;
+    }
 
-    	return (0);
+    /* Continue reading & writing */
+    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        total_written = 0;
+        while (total_written < read_bytes)
+        {
+            written_bytes = write(fd_to, buffer + total_written, read_bytes - total_written);
+            if (written_bytes == -1)
+            {
+                close(fd_from);
+                close(fd_to);
+                error_exit(99, "Error: Can't write to %s\n", argv[2]);
+            }
+            total_written += written_bytes;
+        }
+    }
+
+    if (read_bytes == -1)
+    {
+        close(fd_from);
+        close(fd_to);
+        error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+    }
+
+    if (close(fd_from) == -1)
+        error_exit(100, "Error: Can't close fd %d\n", fd_from);
+
+    if (close(fd_to) == -1)
+        error_exit(100, "Error: Can't close fd %d\n", fd_to);
+
+    return (0);
 }
