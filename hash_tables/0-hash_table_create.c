731415985c63721843ee6b42e4991354a2ca15c5
CodingWithDana <danadaole@gmail.com>
2025-08-04 15:07:13 +0000

    create a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,3 +1,36 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table, or NULL if fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	/* Allocate memory for the hash table structure */
+	/*	(store size and pointer to array */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		/* If malloc fails, return NULL to indicate error */
+		return (NULL);
+	/* Set the size of the hash table */
+	ht->size = size;
+
+	/* Allocate memory for the array (buckets) that */
+	/*		 holds pointers to hash_node_s */
+	/* Initialise to NULL an array of hash_node_t* */
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	/* If allocation fails */
+	if (ht->array == NULL)
+	{
+		/* Free the previously allocated structure */
+		free(ht);
+		/* Return NULL to indicate failure */
+		return (NULL);
+	}
+	/* Return pointer to the newly created hash table */
+	return (ht);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: the key, string
+ * the key is unique in the HashTable
+ * @value: the value corresponding to a key
+ * @next: a pointer to the next node of the list
+ */
+typedef struct hash_node_s
+{
+        char *key;
+        char *value;
+        struct hash_node_s *next;
+} hash_node_t;
+
+
+/**
+ * struct hash_table_s - Hash table data structure
+ * @size: the size of the array
+ * @array: an array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+        unsigned long int size;
+        hash_node_t **array;
+} hash_table_t;
+
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
