aac08c83196888ecebf89a558e7d77a23ddec5da
CodingWithDana <danadaole@gmail.com>
2025-08-05 05:30:58 +0000

    fix betty for 1-djb2.c file

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -6,14 +6,14 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-	/* hash * 33 + c */
-        hash = ((hash << 5) + hash) + c;
-    }
-    return (hash);
+	unsigned long int hash;
+	int c;
+	
+	hash = 5381;
+	while ((c = *str++))
+	{
+		/* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
 }
